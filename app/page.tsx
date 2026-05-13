c0cc3a9f12e037c946996cab12b761b402f86f3c
tchantchasmidias-lab <tchantchasmidias@gmail.com>
2026-05-13 10:47:05 -0300

    style: ajuste de espaçamento superior da Home para 16px (padrão) no mobile

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ function HomeContent() {
 
   return (
     <div className="bg-white min-h-screen font-sans">
-      <div className="pt-[17px] md:pt-4 pb-24 md:pb-10">
+      <div className="pt-4 md:pt-4 pb-24 md:pb-10">
         <div className="w-full px-3 md:px-5">
           
           {/* Carrossel de Categorias */}
