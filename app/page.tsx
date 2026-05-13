0958551b511c1504b82bb0b94777b3ea0f7cd51c
tchantchasmidias-lab <tchantchasmidias@gmail.com>
2026-05-13 11:20:09 -0300

    style: ajuste ultra-fino de espaçamento inferior da Home para 28px no mobile

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ function HomeContent() {
 
   return (
     <div className="bg-white min-h-screen font-sans">
-      <div className="pt-4 md:pt-4 pb-[46px] md:pb-10">
+      <div className="pt-4 md:pt-4 pb-[28px] md:pb-10">
         <div className="w-full px-3 md:px-5">
           
           {/* Carrossel de Categorias */}
